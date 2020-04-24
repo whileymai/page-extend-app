26c06cd036d425723e16438ec596f53ff8e8fefe
whileymai <info@whileymai.com>
2020-04-25 08:42:23 +1000

    Rails and shopify setup

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,15 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = ENV['api_key']
+  config.secret = ENV['api_secret']
+  config.old_secret = ""
+  config.scope = "read_products, read_script_tags, write_script_tags" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.api_version = "2020-04"
+  config.shop_session_repository = 'Shop'
+end
+
+# ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
+# ShopifyAPI::ApiVersion.version_lookup_mode = :raise_on_unknown    # Uncomment to raise an error if attempting to use an api version that was not previously known
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  mount ShopifyApp::Engine, at: '/'
+
+  root to: 'api/v1/home#index'
+  namespace :api do
+    namespace :v1 do
+      get 'home/index'
+    end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
